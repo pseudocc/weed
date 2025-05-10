7d45bd5f449dda097f415cd40ff8d00bbdcd5b59
Atlas Yu <pseudoc@163.com>
2025-05-10 14:43:32 +0800

    build: nix flake init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "WEED";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    inherit (nixpkgs) lib;
+
+    eachSystem = fn:
+      lib.foldl' (
+        acc: system:
+          lib.recursiveUpdate
+          acc
+          (lib.mapAttrs (_: value: {${system} = value;}) (fn system))
+      ) {}
+      lib.platforms.linux;
+    perSystem = system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gcc
+            gnumake
+          ];
+          shellHook = ''
+            export CPATH=$(pwd)
+          '';
+        };
+      };
+  in eachSystem perSystem;
+}
